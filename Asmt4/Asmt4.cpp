c529aaca3160a31f20f41c40c00980a9e96e5ca2
Reily Stanford <reijstan@ut.utm.edu>
2020-09-24 22:13:25 -0500

    Pushing

--- Asmt4/Asmt4.cpp
+++ Asmt4/Asmt4.cpp
@@ -2,6 +2,8 @@
 //
 
 #include <iostream>
+#include "Vector.h"
+
 
 int main()
 {
--- Asmt4/Vector.cpp
+++ Asmt4/Vector.cpp
@@ -1,7 +1,68 @@
 #include "Vector.h"
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
-Vector::Vector() {
+Vector::Vector()
+{
+	ptr = NULL;
+	size = 0;
+	capacity = 0;
+}
 
+Vector::Vector(const Vector& other)
+{
+	size = other.size;
+	capacity = other.capacity;
+	ptr = new int[size];
+	for (int i = 0; i < size; i++) {
+		ptr[i] = other.ptr[i];
+	}
+}
+
+Vector::~Vector()
+{
+	delete [] ptr;
+	capacity = 0;
+	size = 0;
+}
+
+Vector& Vector::operator=(const Vector& other)
+{
+	Vector newvec;
+	newvec.ptr = other.ptr;
+	newvec.capacity = other.capacity;
+	newvec.size = other.size;
+	return newvec;
+}
+
+int Vector::getSize()
+{
+	return 0;
+}
+
+int Vector::getCapacity()
+{
+	return 0;
+}
+
+bool Vector::push(int element)
+{
+	return false;
+}
+
+int Vector::Delete()
+{
+	return 0;
+}
+
+bool Vector::resize()
+{
+	return false;
+}
+
+int& Vector::operator[](unsigned int index)
+{
+	// TODO: insert return statement here
 }
